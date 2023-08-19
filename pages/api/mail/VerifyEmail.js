cae694385ee1227261c736688369b3c8f7f13f34
Dbabalola600 <dami600bab@gmail.com>
2023-08-19 15:19:10 +0100

    email with wrapper

--- pages/api/mail/VerifyEmail.js
+++ pages/api/mail/VerifyEmail.js
@@ -1,31 +1,49 @@
-import { sendEmail } from "../../../utils/sendgrid";
+import { sendMail } from "../../../utils/mailService";
 
 
 
 
-export default async (req, res) => {
-
-    const { method } = req;
-
-    const { mail, title } = JSON.parse(req.body)
-
-    console.log(title)
-
-
+const handler = async (req, res) => {
     try {
-        await sendEmail(
-            //sending to
-            mail,
-            //subject
-            "Mood Verification",
-            ///context
-            "please input the following to verify your account:  " + title
-        );
-        res.status(200).json({ message: 'Email sent successfully' });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Internal Server Error' });
+        const { method } = req;
+
+        const { mail, title } = JSON.parse(req.body)
+
+        console.log(title)
+        switch (method) {
+            case "POST": {
+
+
+                // console.log(mail)
+                //Do some thing
+                await sendMail(
+                    //subject
+                    "Mood Verification",
+                    //mail
+                    mail,
+                    //context
+                   " please input the following to verify your account: "+ title
+
+                );
+                res.status(200).send("Success");
+                break;
+            }
+            case "GET": {
+                //Do some thing
+                res.status(200).send(req.auth_data);
+                break;
+            }
+            default:
+                res.setHeader("Allow", ["POST", "GET", "PUT", "DELETE"]);
+                res.status(405).end(`Method ${method} Not Allowed`);
+                break;
+        }
+    } catch (err) {
+        res.status(400).json({
+            error_code: "api_one",
+            message: err.message,
+        });
     }
+};
 
-
-}
+export default handler;
--- utils/mailService.js
+++ utils/mailService.js
@@ -16,12 +16,16 @@ export async function sendMail(subject, toEmail, otpText) {
     text: otpText,
   };
 
-  transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-      throw new Error(error);
-    } else {
-      console.log("Email Sent");
-      return true;
-    }
-  });
+  
+  await new Promise((resolve, reject) => {
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        throw new Error(error);
+      } else {
+        console.log("Email Sent");
+        return true;
+      }
+    });
+  })
+
 }
