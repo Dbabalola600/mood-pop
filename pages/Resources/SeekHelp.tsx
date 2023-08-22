35a76290a90cea291949c96c45e17825af6607af
Dbabalola600 <dami600bab@gmail.com>
2023-08-22 02:04:32 +0100

    gudda cheese

--- pages/Resources/SeekHelp.tsx
+++ pages/Resources/SeekHelp.tsx
@@ -6,6 +6,16 @@ import DefaultLayout from "../../components/Layout/DefaultLayout";
 
 
 export default function SeekHelp() {
+
+
+    const numbers = [
+        { number: "+2348055014338", name: "me", }
+    ]
+
+
+    const mails = [
+        { mail: "dami600bab@gmail.com", name: "me", }
+    ]
     return (
         <DefaultLayout>
             <div>
@@ -21,10 +31,64 @@ export default function SeekHelp() {
                 <div
                     className="mb-6 text-black text-2xl font-bold mt-5"
                 >
-                    Hotlines
+                    Hotlines📞
                     <div
                         className="w-[150px] bg-primary h-1 rounded-full"
                     />
+                    {numbers.map((info, index) => (
+                        <div>
+                            <div
+                                className="bg-white rounded-lg mt-5 p-5"
+                            >
+
+                                <div>
+                                    {info.name}
+                                </div>
+                                <div>
+                                    <a href={`tel:${info.number}`}>{info.number} 📞</a>
+                                </div>
+
+
+                            </div>
+
+
+                        </div>
+                    ))}
+
+
+
+                </div>
+
+
+                <div
+                    className="mb-6 text-black text-2xl font-bold mt-5"
+                >
+                    Mails ✉️
+                    <div
+                        className="w-[150px] bg-primary h-1 rounded-full"
+                    />
+                    {mails.map((info, index) => (
+                        <div>
+                            <div
+                                className="bg-white rounded-lg mt-5 p-5"
+                            >
+
+                                <div>
+                                    {info.name}
+                                </div>
+                                <div>
+                                    <a href={`mailto:${info.mail}`}>{info.mail} </a>
+                                </div>
+
+
+                            </div>
+
+
+                        </div>
+                    ))}
+
+
+
                 </div>
 
             </div>
