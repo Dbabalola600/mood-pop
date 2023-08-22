c29fb7f4203f31746c8c00fae3b65811c415dac6
Dbabalola600 <dami600bab@gmail.com>
2023-08-22 10:45:46 +0100

    search fix

--- pages/Search/[find].tsx
+++ pages/Search/[find].tsx
@@ -179,7 +179,7 @@ export default function Found() {
 
 
 
-                        {data === undefined ? (
+                        {data[0]?.UserName === undefined ? (
                             <div>
                                 <div>
                                     <Image
